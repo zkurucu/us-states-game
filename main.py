f679a67a3bc3bcf17d19fbb7ef44c2c60b6bdf5e
zkurucu <z.kurucu.d@hotmail.com>
2023-09-05 20:44:27 +0300

    challenge with .csv

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+df = pd.read_csv("50_states.csv")
+states = df["state"].to_list()
+guessed_states = []
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's the another state's name?").title()
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = df[df.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's the another state's name?")
-print(answer_state)
+screen.exitonclick()
